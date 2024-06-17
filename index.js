d792bef32e2b219885ba7baa2737171b43c60d03
Fernando Nava <fernando.nava@upwards.com>
2024-06-17 11:05:40 -0700

    update script to throttle per google sheet rate limit

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ const queryToSheet = require('./lib/query-to-sheet');
       auth: await googleAuth(),
     });
 
-    await Promise.all(queries.map(queryToSheet(googleSheets)));
+    // Function to process batches of queries
+    const processBatch = async (batch) => {
+      await Promise.all(batch.map(queryToSheet(googleSheets)));
+    };
+
+    // Split queries into batches of 50
+    // API says 60 a minute but still beem to be getting throttled
+    const batchSize = 50;
+    for (let i = 0; i < queries.length; i += batchSize) {
+      const batch = queries.slice(i, i + batchSize);
+      await processBatch(batch);
+      if (i + batchSize < queries.length) {
+        // Wait for 70 seconds before processing the next batch
+        await new Promise(resolve => setTimeout(resolve, 70000));
+      }
+    }
   } catch (err) {
     console.log(err);
     throw err;
